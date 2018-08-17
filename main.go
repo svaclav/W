97877c0c637e3072be732e0f728709af13d7090e
Vaclav Strachon <vaclav.strachon@gmail.com>
2018-08-17 21:29:22 +0200

    comments

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import . "wf"
 
 func main() {
-	Π(Σ("hello world"))
-	Π(Δ("Number 5 is more than 2x times greater than number 2"))
+	Π(Σ("hello world"))                                          // HELLO WORLD
+	Π(Δ("Number 5 is more than 2x times greater than number 2")) // 5,2,2
 
 }
--- wf.go
+++ wf.go
@@ -6,13 +6,17 @@ import (
 	"strings"
 )
 
+// print on stdout
 func Π(s string) {
 	fmt.Println(s)
 }
+
+// convert to upper case
 func Σ(s string) string {
 	return strings.ToUpper(s)
 }
 
+// find all numbes in string
 func Δ(s string) string {
 	re := regexp.MustCompile("[0-9]+")
 	return strings.Join(re.FindAllString(s, -1), ",")
