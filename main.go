dcd2a0c2d7da8bc97120d5aa61df9d5f69f1a4b6
Vaclav Strachon <vaclav.strachon@gmail.com>
2018-08-17 19:59:40 +0200

    w functions

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import . "wf"
+
+func main() {
+	Π(Σ("hello world"))
+
+}
---
+++ wf.go
@@ -0,0 +1,13 @@
+package wf
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Π(s string) {
+	fmt.Println(s)
+}
+func Σ(s string) string {
+	return strings.ToUpper(s)
+}
